3ece510226da92a569a73fe9792d0070905a0a6d
nightswinger <stardustkids83@gmail.com>
2025-02-19 02:23:20 +0000

    Add offset, maxWidth, minWidth, maxHeight, and className properties to popup controller

--- src/popup_controller.ts
+++ src/popup_controller.ts
@@ -5,10 +5,22 @@ export default class extends LayerController {
   declare popup: L.Popup
   declare content: string
 
+  declare offsetValue?: L.PointExpression
+  declare maxWidthValue?: number
+  declare minWidthValue?: number
+  declare maxHeightValue?: number
+  declare classNameValue?: string
+
   declare contentValue: string
 
   static values = {
     content: String,
+
+    offset: { type: Array, default: [0, 7] },
+    maxWidth: { type: Number, default: 300 },
+    minWidth: { type: Number, default: 50 },
+    maxHeight: { type: Number, default: null },
+    className: String,
     ...LayerController.values
   }
 
@@ -18,7 +30,14 @@ export default class extends LayerController {
   }
 
   connect() {
-    this.popup = L.popup({ content: this.contentValue || this.content })
+    this.popup = L.popup({
+      offset: this.offsetValue,
+      maxWidth: this.maxWidthValue,
+      minWidth: this.minWidthValue,
+      maxHeight: this.maxHeightValue,
+      className: this.classNameValue,
+      content: this.contentValue || this.content
+    })
     this.dispatch("popup:bindTo", { detail: this.popup, prefix: "leaflet" })
   }
 }

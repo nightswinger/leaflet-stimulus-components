6a5fe965562bc72cef98ba9fd626a28e42475b58
nightswinger <stardustkids83@gmail.com>
2025-02-19 01:28:37 +0000

    Add content initialization to popup controller

--- src/popup_controller.ts
+++ src/popup_controller.ts
@@ -3,6 +3,7 @@ import LayerController from "./layer_controller"
 
 export default class extends LayerController {
   declare popup: L.Popup
+  declare content: string
 
   declare contentValue: string
 
@@ -11,8 +12,13 @@ export default class extends LayerController {
     ...LayerController.values
   }
 
+  initialize(): void {
+    this.content = this.element.innerHTML
+    this.element.innerHTML = ""
+  }
+
   connect() {
-    this.popup = L.popup().setContent(this.contentValue)
+    this.popup = L.popup({ content: this.contentValue || this.content })
     this.dispatch("popup:bindTo", { detail: this.popup, prefix: "leaflet" })
   }
 }

84c7ca96fc6ba70c0e9923b8198fc102a908114a
nightswinger <stardustkids83@gmail.com>
2025-02-21 03:42:53 +0000

    Refactor circle-controller to extend CircleMarkerController

--- src/circle-controller.ts
+++ src/circle-controller.ts
@@ -1,31 +1,11 @@
 import L from "leaflet"
-import LayerController from "./layer-controller"
+import CircleMarkerController from "./circle-marker-controller"
 
-export default class extends LayerController {
+export default class extends CircleMarkerController {
   declare circle: L.Circle
 
-  declare centerValue: L.LatLngExpression
-  declare colorValue: string
-  declare fillColorValue: string
-  declare fillOpacityValue: number
-  declare radiusValue: number
-
-  static values = {
-    center: Array,
-    color: String,
-    fillColor: String,
-    fillOpacity: Number,
-    radius: Number,
-    ...LayerController.values,
-  }
-
   connect() {
-    this.circle = L.circle(this.centerValue, {
-      color: this.colorValue,
-      fillColor: this.fillColorValue,
-      fillOpacity: this.fillOpacityValue,
-      radius: this.radiusValue,
-    })
+    this.circle = L.circle(this.centerValue, this.options)
     this.add(this.circle)
   }
 }

2bc569b95886bee80f0458982d205923d2188ff8
nightswinger <stardustkids83@gmail.com>
2025-02-27 07:55:47 +0000

    Refactor DivIcon class to extend Icon and integrate customizable options

--- src/div-icon.ts
+++ src/div-icon.ts
@@ -1,32 +1,20 @@
-import { Controller } from "@hotwired/stimulus"
 import L from "leaflet"
+import Icon from "./icon"
 
-export default class extends Controller {
+export default class extends Icon {
   declare divIcon: L.DivIcon
 
-  declare iconSizeValue: L.PointExpression
-  declare iconAnchorValue: L.PointExpression
-  declare popupAnchorValue: L.PointExpression
-
   declare htmlValue: string | HTMLElement
-  declare classNameValue: string
 
   static values = {
-    iconSize: { type: Array, default: null },
-    iconAnchor: { type: Array, default: null },
-    popupAnchor: { type: Array, default: [0, 0] },
-  
+    ...Icon.values,
     html: String,
-    className: String,
   }
 
   connect() {
     this.divIcon = L.divIcon({
-      iconSize: this.iconSizeValue,
-      iconAnchor: this.iconAnchorValue,
-      popupAnchor: this.popupAnchorValue,
+      ...this.options,
       html: this.htmlValue,
-      className: this.classNameValue,
     })
     this.dispatch("icon:set", { detail: this.divIcon, prefix: "leaflet" })
   }

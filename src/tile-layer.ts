537e6863350b9362a3d4875c7b5df595f7de3d45
nightswinger <stardustkids83@gmail.com>
2025-02-27 08:47:12 +0000

    Add minZoom and maxZoom options to TileLayer class

--- src/tile-layer.ts
+++ src/tile-layer.ts
@@ -4,14 +4,21 @@ import Layer from "./layer"
 export default class extends Layer {
   declare urlValue: string
 
+  declare minZoomValue: number
+  declare maxZoomValue: number
+
   static values = {
+    ...Layer.values,
     url: String,
-    ...Layer.values
+    minZoom: { type: Number, default: 0 },
+    maxZoom: { type: Number, default: 18 },
   }
 
   connect() {
     this.layer = L.tileLayer(this.urlValue, {
-      attribution :this.attributionValue
+      attribution :this.attributionValue,
+      minZoom: this.minZoomValue,
+      maxZoom: this.maxZoomValue,
     })
     this.add(this.layer)
   }
